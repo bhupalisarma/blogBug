50125d80f927f460599d2c74059089dad170f2c4
Bhupali Sarma <85697658+bhupalisarma@users.noreply.github.com>
2023-02-12 05:54:25 +0530

    MailChimp integrated.

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-
-
+const { startsWith } = require("lodash");
+const https = require("https");
+const request = require("request");
+require("dotenv").config();
 
 const app = express();
+const apiKey = process.env.API_KEY;
 
 app.set("view engine", "ejs");
 
@@ -66,6 +69,45 @@ app.get("/posts/:postName", function (req, res) {
 });
 
 // publish ends
+
+// Contact form starts
+
+app.post("/contact", function (req, res) {
+	const email = req.body.email;
+
+	var data = {
+		members: [
+			{
+				email_address: email,
+				status: "subscribed",
+			},
+		],
+	};
+
+	var jsonData = JSON.stringify(data);
+
+	const url = "https://us13.api.mailchimp.com/3.0/lists/5ab3f35274";
+	const options = {
+		method: "post",
+		auth: `user:${apiKey}`,
+	};
+
+	const request = https.request(url, options, function (response) {
+		if (response.statusCode === 200) {
+			res.redirect("/");
+		} else {
+			res.redirect("/");
+		}
+		// response.on("data",function(data){
+		//     console.log(JSON.parse(data));
+		// })
+	});
+
+	request.write(jsonData);
+	request.end();
+});
+
+// contactform ends
 
 app.listen(3000, function () {
 	console.log("Server started on port 3000");
